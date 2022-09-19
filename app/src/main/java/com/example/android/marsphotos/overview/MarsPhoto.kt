c0b61a48d576df2e36b184b8c77ff8f7340f0762
Saksham mittal <mittal.saksham2001@gmail.com>
2022-09-19 11:05:30 +0530

    sample5 commit

---
+++ app/src/main/java/com/example/android/marsphotos/overview/MarsPhoto.kt
@@ -0,0 +1,9 @@
+package com.example.android.marsphotos.overview
+
+import com.squareup.moshi.Json
+
+data class MarsPhoto(
+    val id: String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
+    @Json(name = "img_src") val imgSrcUrl: String
+)
---
+++ app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.android.marsphotos.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import kotlinx.coroutines.launch
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<MarsApiStatus>()
+    val status: LiveData<MarsApiStatus> = _status
+    // The external immutable LiveData for the request status
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+    val photos: LiveData<List<MarsPhoto>> = _photos
+
+    init {
+        getMarsPhotos()
+    }
+
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
+    }
+}
